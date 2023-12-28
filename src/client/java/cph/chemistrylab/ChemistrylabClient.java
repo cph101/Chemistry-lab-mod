d021c84f2043e826b96182cc93376f8795167329
cph101 <cph101@icloud.com>
2023-12-28 13:53:39 +0000

    Initial Commit

---
+++ src/client/java/cph/chemistrylab/ChemistrylabClient.java
@@ -0,0 +1,10 @@
+package cph.chemistrylab;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ChemistrylabClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/cph/chemistrylab/ChemistrylabDataGenerator.java
@@ -0,0 +1,11 @@
+package cph.chemistrylab;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ChemistrylabDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
