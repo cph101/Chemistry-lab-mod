935a41e3d070e98588dfad1f15deda8deb39fa78
cph101 <cph101@icloud.com>
2024-01-01 20:48:17 +0000

    Added ColorProvider and tweaked gradle

--- src/client/java/cph/chemistrylab/ChemistrylabClient.java
+++ src/client/java/cph/chemistrylab/ChemistrylabClient.java
@@ -1,13 +1,14 @@
 package cph.chemistrylab;
 
+import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
+
 public class ChemistrylabClient implements ClientModInitializer {
 
 	@Override
 	public void onInitializeClient(ModContainer mod) {
-
-		// what a shame this doesn't work with quilt
-
+		ColorProviderRegistry.ITEM.register((stack, tintIndex) -> tintIndex == 0 ? 0x01ABDD : -1, ModItems.FILLED_BEAKER);
+		ColorProviderRegistry.ITEM.register((stack, tintIndex) -> tintIndex == 0 ? 0x01ABDD : -1, ModItems.FILLED_TEST_TUBE);
 	}
 }
