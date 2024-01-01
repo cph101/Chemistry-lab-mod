aa870f18e25e090dd828176d154cdcb1c87bcd06
cph101 <cph101@icloud.com>
2024-01-01 14:08:56 +0000

    Port to quilt

--- src/client/java/cph/chemistrylab/ChemistrylabClient.java
+++ src/client/java/cph/chemistrylab/ChemistrylabClient.java
@@ -1,23 +1,24 @@
 package cph.chemistrylab;
 
-import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
-import net.minecraft.item.Item;
+/*import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.util.registry.RegistryEntry;
+import net.minecraft.util.registry.RegistryEntry;*/
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
 
 public class ChemistrylabClient implements ClientModInitializer {
 
 	@Override
-	public void onInitializeClient() {
-
-		ColorProviderRegistry.ITEM.register((stack, tintIndex) -> {
+	public void onInitializeClient(ModContainer mod) {
+		/*ColorProviderRegistry.ITEM.register((stack, tintIndex) -> {
 			return tintIndex == 0 ? 0x01ABDD : -1;
 		}, ModItems.FILLED_BEAKER);
 
 		ColorProviderRegistry.ITEM.register((stack, tintIndex) -> {
 			return tintIndex == 0 ? 0x01ABDD : -1;
-		}, ModItems.FILLED_TEST_TUBE);
+		}, ModItems.FILLED_TEST_TUBE); */
+
+		// what a shame this doesn't work with quilt
 	}
 }
--- src/main/java/cph/chemistrylab/ModItems.java
+++ src/main/java/cph/chemistrylab/ModItems.java
@@ -2,26 +2,26 @@ package cph.chemistrylab;
 
 import cph.chemistrylab.items.CustomBeakerItem;
 import cph.chemistrylab.items.CustomTestTubeItem;
-import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import org.quiltmc.qsl.item.group.api.QuiltItemGroup;
+import org.quiltmc.qsl.item.setting.api.QuiltItemSettings;
 
 public class ModItems {
     // mod items
 
 
-    public static final Item BEAKER = Registry.register(Registry.ITEM, new Identifier("chemistrylab", "beaker"), new Item(new FabricItemSettings().maxCount(16)));
-    public static final Item FILLED_BEAKER = Registry.register(Registry.ITEM, new Identifier("chemistrylab", "filled_beaker"), new CustomBeakerItem(new FabricItemSettings().maxCount(1)));
-    public static final Item TEST_TUBE = Registry.register(Registry.ITEM, new Identifier("chemistrylab", "test_tube"), new Item(new FabricItemSettings().maxCount(8)));
-    public static final Item FILLED_TEST_TUBE = Registry.register(Registry.ITEM, new Identifier("chemistrylab", "filled_test_tube"), new CustomTestTubeItem(new FabricItemSettings().maxCount(8)));
+    public static final Item BEAKER = Registry.register(Registry.ITEM, new Identifier("chemistrylab", "beaker"), new Item(new QuiltItemSettings().maxCount(16)));
+    public static final Item FILLED_BEAKER = Registry.register(Registry.ITEM, new Identifier("chemistrylab", "filled_beaker"), new CustomBeakerItem(new QuiltItemSettings().maxCount(1)));
+    public static final Item TEST_TUBE = Registry.register(Registry.ITEM, new Identifier("chemistrylab", "test_tube"), new Item(new QuiltItemSettings().maxCount(8)));
+    public static final Item FILLED_TEST_TUBE = Registry.register(Registry.ITEM, new Identifier("chemistrylab", "filled_test_tube"), new CustomTestTubeItem(new QuiltItemSettings().maxCount(8)));
     // Custom Item Group
-    @SuppressWarnings({"unused"}) public static final ItemGroup CHEMISTRY_GROUP = FabricItemGroupBuilder.create(
+    @SuppressWarnings({"unused"}) public static final ItemGroup CHEMISTRY_GROUP = QuiltItemGroup.builder(
             new Identifier(Chemistrylab.MOD_ID, "chemistry_group"))
-            .icon(() -> new ItemStack(FILLED_BEAKER)) // set icon to default beaker
+            .icon(() -> new ItemStack(FILLED_BEAKER))
             .appendItems(stacks -> {
                 stacks.add(new ItemStack(BEAKER));
                 stacks.add(new ItemStack(TEST_TUBE));
