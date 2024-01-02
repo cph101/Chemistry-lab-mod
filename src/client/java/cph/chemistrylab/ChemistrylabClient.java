77dd5f8a3aba2112e239b799a7412dade031855b
cph101 <cph101@icloud.com>
2024-01-02 11:44:47 +0000

    Update ChemistrylabClient.java

--- src/client/java/cph/chemistrylab/ChemistrylabClient.java
+++ src/client/java/cph/chemistrylab/ChemistrylabClient.java
@@ -1,18 +1,21 @@
 package cph.chemistrylab;
 
 import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
+import net.minecraft.item.ItemStack;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
 
 public class ChemistrylabClient implements ClientModInitializer {
 
-	// Insert color getting function here
+	public Integer getItemTint(ItemStack stack) {
+		return 0x01ABDD;
+		// This will eventually changed based on element
+	}
 
 	@Override
 	public void onInitializeClient(ModContainer mod) {
 
-		// Git testing
-		ColorProviderRegistry.ITEM.register((stack, tintIndex) -> tintIndex == 0 ? 0x01ABDD : -1, ModItems.FILLED_BEAKER);
-		ColorProviderRegistry.ITEM.register((stack, tintIndex) -> tintIndex == 0 ? 0x01ABDD : -1, ModItems.FILLED_TEST_TUBE);
+		ColorProviderRegistry.ITEM.register((stack, tintIndex) -> tintIndex == 0 ? getItemTint(stack) : -1, ModItems.FILLED_BEAKER);
+		ColorProviderRegistry.ITEM.register((stack, tintIndex) -> tintIndex == 0 ? getItemTint(stack) : -1, ModItems.FILLED_TEST_TUBE);
 	}
 }
